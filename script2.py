691f7c899777065c68f9b4ce4e64afdbebeeacd5
agajipara <ayushgajipara846@gmail.com>
2024-05-27 13:37:36 -0400

    Modified both files in the repo clone
    - Deleted a print() statement from script1.py
    - Added a print() statement to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # Print lines from Zen of Python
 print("Special cases aren't special enough to break the rules.")
 print("Although practicality beats purity.")
+print("Flat is better than nested.")
