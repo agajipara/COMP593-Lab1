548d2a4c959fb6411607f47c6e0d8fa3624b6758
agajipara <ayushgajipara846@gmail.com>
2024-05-27 13:08:29 -0400

    Modified both script files
    - Deleted print from script1.py
    - Added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("Complex is better than complicated.")
-print("Flat is better than nested.")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# Print lines from Zen of Python
 print("Special cases aren't special enough to break the rules.")
 print("Although practicality beats purity.")
